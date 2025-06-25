5ca4c1b54859fcbf2a6226341dc50a4e05a98d6a
Savinov Stanislav <savinov.stas2001@gmail.com>
2025-06-25 20:04:26 +0300

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -2,11 +2,11 @@ import Redis from "ioredis";
 
 const redisUrl = process.env.REDIS_URL || "redis://default:********@ballast.proxy.rlwy.net:49795";
 
-const redisClient = new Redis(redisUrl, {
-  maxRetriesPerRequest: null,
-  enableReadyCheck: false,
-  tls: redisUrl.startsWith('rediss://') ? {} : undefined
-});
+const redisClient = new Redis(
+  process.env.REDIS_URL + '?family=0',
+  { maxRetriesPerRequest: null, enableReadyCheck: false, tls: redisUrl.startsWith('rediss://') ? {} : undefined }
+);
+
 
 // Логирование для отладки
 console.log(`Connecting to Redis at: ${redisUrl.replace(/:.*@/, ':********@')}`);
