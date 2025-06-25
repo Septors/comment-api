b77945d7b47321e79a69251ee53d71e3ad6e05aa
Savinov Stanislav <savinov.stas2001@gmail.com>
2025-06-25 19:51:47 +0300

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,14 +1,9 @@
 import Redis from "ioredis";
 
-const redisConfig = {
-  host: process.env.REDIS_HOST || "localhost",
-  port: parseInt(process.env.REDIS_PORT || "6379"),
-  password: process.env.REDIS_PASSWORD, // если требуется
-  maxRetriesPerRequest: null, // важно для BullMQ
+const redisClient = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
   enableReadyCheck: false,
-};
-
-const redisClient = new Redis(redisConfig);
+});
 
 redisClient.on("error", (err) => {
   console.error("Redis error:", err);
